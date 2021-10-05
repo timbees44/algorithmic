3396ce6b2fbcd1e31090e13cc22385823f2e4cb2
tim <tburrey@protonmail.com>
2021-10-05 20:22:57 +0100

    insertion sort

---
+++ algorithmic/main.py
@@ -0,0 +1,19 @@
+class algos(object):
+ 
+
+    # insertion sort algorithm
+    def insertion_sort(self, lst):
+        for i in range(0, len(lst)):
+            print(lst[i])
+            currentVal = lst[i]
+            currentPos = i
+
+
+
+
+
+array = [20, 42, 12, 65, 45, 63, 72, 14, 25, 74]
+
+
+
+algos().insertion_sort(array)
