542f43bad7c5b206504924afd065f32693faae39
tim <tburrey@protonmail.com>
2021-10-05 22:02:22 +0100

    insertion done, needs commenting

--- algorithmic/main.py
+++ algorithmic/main.py
@@ -7,13 +7,14 @@ class algos(object):
             print(lst[i])
             currentVal = lst[i]
             currentPos = i
-
-
-
+            
+            while currentPos > 0 and lst[currentPos - 1] > currentVal:
+                lst[currentPos] = lst[currentPos - 1]
+                currentPos = currentPos - 1
+            
+            lst[currentPos] = currentVal
 
 
 array = [20, 42, 12, 65, 45, 63, 72, 14, 25, 74]
-
-
 
 algos().insertion_sort(array)
