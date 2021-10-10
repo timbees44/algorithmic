8e2a01e898a91f8cedc09a05d7e98f8722359fb7
tim <tburrey@protonmail.com>
2021-10-10 14:43:05 +0100

    insertion, notes added

--- algorithmic/main.py
+++ algorithmic/main.py
@@ -1,22 +1,30 @@
 class algos(object):
  
-
-    # insertion sort algorithm
-    def insertion_sort(self, lst):
+    # indexnsertion sort algorithm
+    '''
+    - O(n**2) time complexity
+    - Good for small arrays and nearly sorted arrays
+    - how insertion works
+        - each value (n) in array is assessed
+        - if n < index(n-1) then all values in array move right
+    '''
+    def insertion_sort(self, array):
         print("original list")
-        print(lst)
-        for i in range(0, len(lst)):
-            currentVal = lst[i]
-            currentPos = i
+        print(array)
+        for index in range(0, len(array)):
+            currentVal = array[index]
+            currentPos = index
             
-            while currentPos > 0 and lst[currentPos - 1] > currentVal:
-                lst[currentPos] = lst[currentPos - 1]
+            while currentPos > 0 and array[currentPos - 1] > currentVal:
+                array[currentPos] = lst[currentPos - 1]
                 currentPos = currentPos - 1
             
-            lst[currentPos] = currentVal
+            array[currentPos] = currentVal
         print("sorted list")
-        print(lst)
+        print(array)
 
 array = [20, 42, 12, 65, 45, 63, 72, 14, 25, 74]
+
+    
 
 algos().insertion_sort(array)
